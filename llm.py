316e84b9a6adc721441024cdc8ed4dfef5bdf8ba
Amloner <124287518+Amloner@users.noreply.github.com>
2024-06-25 00:45:45 +0530

    Update llm.py
    Removed embedding vector feature

--- llm.py
+++ llm.py
@@ -9,12 +9,3 @@ llm = ChatOpenAI(
     model=st.secrets["OPENAI_MODEL"],
 )
 # end::llm[]
-
-# tag::embedding[]
-# Create the Embedding model
-from langchain_openai import OpenAIEmbeddings
-
-embeddings = OpenAIEmbeddings(
-    openai_api_key=st.secrets["OPENAI_API_KEY"]
-)
-# end::embedding[]
